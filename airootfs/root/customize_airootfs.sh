202acfabf506187cb1ea45d408c8385ca72b7f12
Davi <davi.silva@live.com>
2020-01-19 03:22:57 -0300

    Basic GUI and packages set up

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -7,6 +7,7 @@ locale-gen
 
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
+echo -e "[Icon Theme]\nInherits=breeze_cursors" > /usr/share/icons/default/index.theme
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
@@ -19,5 +20,7 @@ sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
+systemctl enable NetworkManager
+systemctl enable sddm.service
 systemctl enable pacman-init.service choose-mirror.service
-systemctl set-default multi-user.target
+systemctl set-default graphical.target
